6f7cbaf136da971a8aa17295f3fd4d149d39f522
Timofei Gladchenko <148538071+trueicecannon@users.noreply.github.com>
2024-10-27 21:22:14 +0400

    Add files via upload

--- src/ClientApp.java
+++ src/ClientApp.java
@@ -8,17 +8,15 @@ public class ClientApp {
         String host = "localhost"; // или "127.0.0.1"
         int port = 8080;
 
-        try {
-            Socket socket = new Socket(host, port);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        try (Socket socket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
             out.println("Alice");
 
             String response = in.readLine();
             System.out.println("Server response: " + response);
 
-            socket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/ServerApp.java
+++ src/ServerApp.java
@@ -8,23 +8,22 @@ public class ServerApp {
     public static void main(String[] args) {
         int port = 8080;
 
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server started. Waiting for connections...");
 
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("New connection accepted");
+            try (Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
 
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("New connection accepted");
+                String name = in.readLine();
+                System.out.println("Received: " + name);
 
-            String name = in.readLine();
-            System.out.println("Received: " + name);
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
 
-            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-
-            clientSocket.close();
-            serverSocket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
